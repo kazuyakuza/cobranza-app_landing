f8f13c9f7828be37c3ec4e9f92485949e85e1d13
JEB <jebarrocal@gmail.com>
2026-05-26 12:29:11 -0300

    feat: add Bootstrap 5 and Bootstrap Icons configuration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 const app = createApp(App)
 app.mount('#app')
